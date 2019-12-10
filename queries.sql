373c2b7986ea7309966f2a7328939fea1235e614
allensam88 <54912919+allensam88@users.noreply.github.com>
2019-12-10 13:27:47 -0800

    modified queries for northwinds db file instead of W3 browser

--- queries.sql
+++ queries.sql
@@ -1,7 +1,7 @@
 -- Database Queries
 
 -- Find all customers with postal code 1010
-SELECT * FROM [Customers]
+SELECT * FROM [Customer]
 WHERE PostalCode = 1010
 --CustomerID	CustomerName	            ContactName	        Address	                                    City	        PostalCode	Country
 --12	        Cactus Comidas para llevar	Patricio Simpson	Cerrito 333	                                Buenos Aires	1010	    Argentina
@@ -10,39 +10,39 @@ WHERE PostalCode = 1010
 
 
 -- Find the phone number for the supplier with the id 11
-SELECT SupplierName, Phone FROM [Suppliers]
-WHERE SupplierID = 11 
+SELECT CompanyName, Phone FROM [Supplier]
+WHERE Id = 11 
 -- SupplierName: Heli Süßwaren GmbH & Co. KG, Phone: (010) 9984510
 
 
 -- List first 10 orders placed, sorted descending by the order date
-SELECT * FROM [Orders]
+SELECT * FROM [Order]
 ORDER BY OrderDate DESC
 LIMIT 10
 
 
 -- Find all customers that live in London, Madrid, or Brazil
-SELECT * FROM [Customers]
+SELECT * FROM [Customer]
 WHERE City = 'London' OR 'Madrid' OR Country = 'Brazil'
 --number of records returned: 18
 
 
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
-INSERT INTO Customers (CustomerName, ContactName, Address, City, PostalCode, Country)
+INSERT INTO Customer (CompanyName, ContactName, Address, City, PostalCode, Country)
 VALUES ('The Shire', 'Bilbo Baggins', '1 Hobbit-Hole', 'Bag End', '111', 'Middle Earth');
 
 
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
-UPDATE Customers
+UPDATE Customer
 SET PostalCode = '11122'
-WHERE CustomerID = 92;
+WHERE Id = 92;
 
 
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
-SELECT DISTINCT City FROM [Customers]
+SELECT DISTINCT City FROM [Customer]
 
 
 -- (Stretch) Find all suppliers who have names longer than 20 characters. You can use `length(SupplierName)` to get the length of the name
-SELECT SupplierName FROM [Suppliers]
-WHERE length(SupplierName) > 20
+SELECT CompanyName FROM [Supplier]
+WHERE length(CompanyName) > 20
 --number of records returned: 11
